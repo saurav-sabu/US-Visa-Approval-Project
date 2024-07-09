f400ff68111f7c392c750496c2f97e74eec397f3
Saurav Sabu <saurav.sabu9@gmail.com>
2024-07-10 00:03:57 +0530

    Added comments for template.py file

--- template.py
+++ template.py
@@ -1,8 +1,10 @@
 import os
 from pathlib import Path
 
+# Define the project name
 project_name = "us_visa"
 
+# List of files to be created for the project
 list_of_files = [
     f"{project_name}/__init__.py",
 
@@ -42,17 +44,24 @@ list_of_files = [
 ]
 
 
+# Loop through each file path in the list
 for file_path in list_of_files:
 
+    # Convert the file path string to a Path object
     file_path = Path(file_path)
+
+    # Split the file path into directory and file name
     file_dir, file_name = os.path.split(file_path)
 
+    # Create the directory if it does not exist
     if file_dir != "":
         os.makedirs(file_dir, exist_ok=True)
 
+    # Create an empty file if it does not exist or if it is empty
     if (not os.path.exists(file_path)) or (os.path.getsize(file_path)==0):
         with open(file_path,"w") as f:
             pass
     else:
+        # Print a message if the file already exists
         print(f"file already exists: {file_path}")
     
