69a1eb56635a9f6ca87b6ce21e634b096815ec06
Saurav Sabu <saurav.sabu9@gmail.com>
2024-07-10 00:43:23 +0530

    Added comments for setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup,find_packages
 
+# Setup configuration for the project
 setup(
-    name="US Visa Approval",
-    version="0.0.0",
-    author="Saurav Sabu",
-    author_email="saurav.sabu9@gmail.com",
+    name="US Visa Approval", # Name of the project
+    version="0.0.0", # Version of the project
+    author="Saurav Sabu", # Author's name
+    author_email="saurav.sabu9@gmail.com", # Author's email
+    # Automatically find and include all packages in the project
     packages=find_packages()
 )
