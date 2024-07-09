0cfb11963a876cdae8399ca9177c083db5dd5fa4
Saurav Sabu <saurav.sabu9@gmail.com>
2024-07-10 00:30:46 +0530

    Added code in setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="US Visa Approval",
+    version="0.0.0",
+    author="Saurav Sabu",
+    author_email="saurav.sabu9@gmail.com",
+    packages=find_packages()
+)
